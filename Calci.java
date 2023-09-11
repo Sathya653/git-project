82e9154d2397954e397c0a18ec6bd593a918ce3f
sathya653 <sathya93800@gmail.com>
2023-09-11 15:19:42 +0530

    added driver class

--- Calci.java
+++ Calci.java
@@ -9,3 +9,13 @@ class Calciimp implements Calci
 	return a+b;
 }
 }
+class CalciDriver{
+	public static void main(String[] args){
+	Scanner s = new Scanner(System.in);
+	System.out.println("Enter 2 numbers to print sum");
+	int n1 = s.nextInt();
+	int n2 = s.nextInt();
+	Calciimp i = new Calciimp();
+	System.out.println(n1+"+"+n2+"="+i.Sum(n1,n2);
+}
+}
